6787a99424914748b7a5a5a3f0917e97d7288312
TheGoldenWorld <dawnoftime.inc@gmail.com>
2025-11-20 19:11:32 +0100

    First structure generating - FIRST TRY MABOYYYYY

---
+++ common/src/main/java/jvs/Constants.java
@@ -0,0 +1,11 @@
+package jvs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "jvs";
+	public static final String MOD_NAME = "jakivanillastructures";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ fabric/src/main/java/jvs/jvs.java
@@ -0,0 +1,13 @@
+package jvs;
+
+import net.fabricmc.api.ModInitializer;
+
+public class jvs implements ModInitializer {
+    
+    @Override
+    public void onInitialize() {
+        // This method is invoked by the Fabric mod loader when it is ready
+        // to load your mod. You can access Fabric and Common code in this
+        // project.
+    }
+}
