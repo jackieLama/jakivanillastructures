299bf232c9eb5c0a6a5e6a4d95ca212f17f7a333
TheGoldenWorld <dawnoftime.inc@gmail.com>
2026-06-05 16:38:44 +0200

    1.21.1 portage of the 1.20.1

---
+++ neoforge/src/main/java/jvs/jvs.java
@@ -0,0 +1,12 @@
+package jvs;
+
+import net.neoforged.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class jvs {
+
+    public jvs() {
+        // This method is invoked by the NeoForge mod loader when it is ready
+        // to load your mod.
+    }
+}
